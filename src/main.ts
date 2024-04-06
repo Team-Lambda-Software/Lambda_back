885f9321737107f771dbaea4e9bf554b7075f02d
Fussita <hualong.chiang@gmail.com>
2024-04-06 18:10:20 -0400

    develop branch

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   if ( !process.env.PORT ) {
     console.log(' > .env not found it')
+  } else {
+    console.log(' > .env found it')
   }
   await app.listen(process.env.PORT || 7001);
 }
