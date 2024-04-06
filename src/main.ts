5fc5c4d4faf5e59b4354246225a281a0df423c10
Fussita <hualong.chiang@gmail.com>
2024-04-06 12:04:34 -0400

    init_commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  if ( !process.env.PORT ) {
+    console.log(' > .env not found it')
+  }
+  await app.listen(process.env.PORT || 7001);
+}
+bootstrap();
