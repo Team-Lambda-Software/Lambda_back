043e1e8d0b3c456708ee4986474609f7f95d396e
Samuel <Samuelalonso939@gmail.com>
2024-04-29 18:08:57 -0400

    Reapply "Merge pull request #7 from Team-Lambda-Software/user-module"
    This reverts commit 552eb4c81181bf2639afbcc04226ddc2d1dd7caa.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TestController } from './test/infraestructure/controller/test.controller'
+import { ormDatabaseProvider } from './common/Infraestructure/providers/db-providers/db-provider'
+import { UserController } from './user/infraestructure/controller/user.controller'
 
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
   ],
-  controllers: [TestController],
-  providers: [],
+  controllers: [TestController, UserController],
+  providers: [ ormDatabaseProvider],
 })
 export class AppModule {}
---
+++ src/user/domain/repositories/user-repository.interface.ts
@@ -0,0 +1,14 @@
+import { Result } from "src/common/Application/result-handler/Result"
+import { User } from "../user"
+
+
+
+export interface IUserRepository
+{
+
+    saveUserAggregate ( user: User ): Promise<Result<User>>
+    findUserByEmail ( email: string ): Promise<Result<User>>
+    findUserById ( id: string ): Promise<Result<User>>
+    updateUserPassword ( email:string, newPassword:string ): Promise<Result<User>>
+    
+}
