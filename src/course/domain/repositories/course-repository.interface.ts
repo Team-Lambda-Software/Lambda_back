a4e76baa7628fa29b329653d15f4147676434ca7
Samuel <Samuelalonso939@gmail.com>
2024-04-29 21:44:33 -0400

    add find course by category to the course repository interface

--- src/course/domain/repositories/course-repository.interface.ts
+++ src/course/domain/repositories/course-repository.interface.ts
@@ -11,5 +11,7 @@ export interface ICourseRepository {
     //si hacemos esto por partes tendriamos que buscar las secciones por separado, traer los comentarios de una vez (estilo udemy)
     findCourseSections( id: string ): Promise<Result<Section[]>>
     addCommentToSection( sectionId: string, comment: Comment ): void
+    //suponiendo que esto se vaya a hacer por separado de los blogs
+    findCoursesByCategory( categoryId: string ): Promise<Result<Course[]>>
 
 }
