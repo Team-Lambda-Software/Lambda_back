0c2ae9800598c4b1547b14db66fc4cb4d9ba1f5c
Samuel <Samuelalonso939@gmail.com>
2024-05-06 08:10:46 -0400

    course relation with trainer

--- src/course/infraestructure/entities/orm-entities/orm-course.ts
+++ src/course/infraestructure/entities/orm-entities/orm-course.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, ManyToOne, OneToMany, OneToOne, PrimaryColumn } from "typeorm"
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryColumn } from "typeorm"
 import { OrmSection } from "./orm-section"
 import { OrmSectionImage } from "./orm-section-images"
 import { OrmTrainer } from '../../../../trainer/infraestructure/entities/orm-entities/trainer.entity';
@@ -24,10 +24,10 @@ export class OrmCourse
 
     //TODO relacion con trainer y con categoria
     @Column( { type: "uuid", nullable: true } ) trainer_id: string
-    @ManyToOne( () => OrmTrainer, trainer => trainer.courses ) trainer: OrmTrainer
+    @ManyToOne( () => OrmTrainer, trainer => trainer.courses ) @JoinColumn({ name: 'trainer_id'}) trainer: OrmTrainer
 
     @Column( { type: "uuid", nullable: true } ) category_id: string
-    // @ManyToOne( () => OrmCategory, category => category.courses ) category: OrmCategory
+    // @ManyToOne( () => OrmCategory, category => category.courses ) @JoinColumn({ name: 'category_id'}) category: OrmCategory
 
     static create ( id: string, name: string, description: string, level: number, weeks_duration: number, minutes_per_section: number ): OrmCourse
     {
