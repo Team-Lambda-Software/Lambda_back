565e0c94c98df9f96681a6b363b148d5bf6461c0
Samuel <Samuelalonso939@gmail.com>
2024-05-06 22:09:35 -0400

    search courses by levels endpoint

--- src/course/infraestructure/repositories/orm-repositories/orm-couser-repository.ts
+++ src/course/infraestructure/repositories/orm-repositories/orm-couser-repository.ts
@@ -41,6 +41,31 @@ export class OrmCourseRepository extends Repository<OrmCourse> implements ICours
         this.ormVideoRepository = dataSource.getRepository( OrmSectionVideo )
         this.ormCommentRepository = dataSource.getRepository( OrmSectionComment )
     }
+    async findCoursesByLevels ( levels: number[], pagination: PaginationDto ): Promise<Result<Course[]>>
+    {
+        try
+        {
+            const courses = await this.createQueryBuilder( 'course' ).leftJoinAndSelect('course.trainer','trainer').where( 'course.level IN (:...levels)', { levels:  levels }  ).skip( pagination.offset ).take( pagination.limit ).getMany()
+            
+            if ( courses.length > 0 )
+            {
+
+                for ( const course of courses )
+                {
+                    const sections = await this.ormSectionRepository.findBy( { course_id: course.id } )
+                    course.sections = sections
+                    const courseImage = await this.ormImageRepository.findOneBy( { course_id: course.id } )
+                    course.image = courseImage
+
+                }
+                return Result.success<Course[]>( await Promise.all( courses.map( async course => await this.ormCourseMapper.fromPersistenceToDomain( course ) ) ), 200 )
+            }
+            return Result.fail<Course[]>( new Error( 'Courses not found' ), 404, 'Courses not found' )
+        } catch ( error )
+        {
+            return Result.fail<Course[]>( new Error( error.detail ), error.code, error.detail )
+        }
+    }
 
     async findAllTrainerCourses ( trainerId: string, pagination: PaginationDto ): Promise<Result<Course[]>>
     {
