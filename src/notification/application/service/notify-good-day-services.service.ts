4bb38ab1a510692ae8b423102776e8dc660220c1
Fussita <hualong.chiang@gmail.com>
2024-06-11 22:10:28 -0400

    correction: not_module

--- src/notification/application/service/notify-good-day-services.service.ts
+++ src/notification/application/service/notify-good-day-services.service.ts
@@ -31,15 +31,16 @@ export class NotifyGoodDayApplicationService implements IApplicationService<Appl
         const listTokens = findResult.Value
 
         listTokens.forEach( async e => {  
+            
+            this.notiAlertRepository.saveNotificationAlert(
+                OrmNotificationAlert.create( 
+                    await this.uuidGenerator.generateId(), e.user_id, "Good new Day!", 'be Happy, my budy', false, new Date() )
+            )
+            
             try {
-                const pushMessage:PushNotificationDto = { token: e.token, notification: { title: 'Good ney day!', body: 'be Happy, my budy' } }    
+                const pushMessage:PushNotificationDto = { token: e.token, notification: { title: 'Good new day!', body: 'be Happy, my budy' } }    
                 const result = await this.pushNotifier.sendNotification( pushMessage )
-                if ( result.isSuccess() ) {
-                    this.notiAlertRepository.saveNotificationAlert(
-                        OrmNotificationAlert.create( 
-                            await this.uuidGenerator.generateId(), e.user_id, "Good new Day!", 'be Happy, my budy', false, new Date() )
-                    )
-                }
+                //if ( result.isSuccess() ) {}
             } catch (e) { return Result.fail(new Error('Something went wrong'), 500, 'Something went wrong') }
         })
         return Result.success('good day push sended', 200)
